8ba827ea9dd86df7251d60cbbd46a20a9a0052e2
etrnal70 <etrnal70@gmail.com>
2020-07-04 13:56:18 +0700

    Random

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+extern crate box_drawing;
+
+struct Animal{
+    age : i32,
+    size : i32,
+    name : String,
+}
+
+struct Person {
+    name : String,
+    age : i32,
+}
+
 fn main() {
     println!("Hello, world!");
+    test();
+
+
+    let lion = Animal{
+        age : 12,
+        size : 50,
+        name : String::from("Bro"),
+    };
+
+    match lion {
+        Animal{age,size,name} => {
+            println!("Age : {}, Size : {}, Name : {}",age, name, size);
+        },
+    }
+
+
+fn test(){
+    let z = 32;
+    let x = 64;
+    
+    let res = sum(z, x);
+
+    println!("Test = {}", res);
+}
+
+fn sum(a : i32, b : i32) -> i32{
+    a + b
+}
+
+fn multiply(x : f32, y : f32) -> f32 {
+    x * y
 }
